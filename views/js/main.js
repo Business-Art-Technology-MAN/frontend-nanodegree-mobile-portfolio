5a02b626544550c82063f8bc48e3a3d105b70315
Joe Mitchell <jmitchell@efficient.com>
2018-04-08 21:33:51 -0500

    final project clean up

--- views/js/main.js
+++ views/js/main.js
@@ -448,12 +448,31 @@ var resizePizzas = function(size) {
   }
 
   // Iterates through pizza elements on the page and changes their widths
+  //
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+    /*for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
-    }
+    } */
+
+    //BAT-MAN call quearySelctorAll onces and assign variable and move layout out of loop
+    
+    var qsaPizzasCollection = document.querySelectorAll(".randomPizzaContainer");
+    var pizzaArray = Array.prototype.slice.apply(qsaPizzasCollection);
+    //avoid memory allocation in loops when you can
+    var dx =0;
+    var arrWidth = [];
+    //read properties and calculate and store new width
+    pizzaArray.forEach(function(elem, index, arr) {
+       dx = determineDx(elem, size);
+       arrWidth.push((elem.offsetWidth + dx) + 'px');
+      //arr[index].style.width = newwidth;
+    });
+    //write new widths
+    pizzaArray.forEach(function(elem, index, arr) {
+      arr[index].style.width = arrWidth[index];
+   });
   }
 
   changePizzaSizes(size);
@@ -501,13 +520,23 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    // document.body.scrollTop is no longer supported in Chrome.
+  var items = Array.prototype.slice.apply(document.querySelectorAll('.mover'));
+  // document.body.scrollTop is no longer supported in Chrome.
+  
+  var phase = 0.0;
+  var arrNewPosition=[];
+  //read loop
+  items.forEach(function(elem){
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var phase = Math.sin((scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-  }
+    phase = Math.sin((scrollTop / 1250) + (i % 5));
+    arrNewPosition.push(elem.basicLeft + 100 * phase + 'px');
+  });
+
+  //write loop
+  items.forEach(function(elem, index, arr){
+    
+    arr[index].style.left = arrNewPosition[index];
+  });
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
